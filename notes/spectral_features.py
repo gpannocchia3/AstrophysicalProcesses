7e3ac9dfe5bc9dc7ab8a80c2ecff2efb88fd576f
Giacomo Pannocchia <giacomopannocchia@MacBook-Air-di-Giacomo.local>
2025-11-08 17:44:49 +0100

    Added some more cool images

--- notes/spectral_features.py
+++ notes/spectral_features.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif"
+})
 
 # Constants
 h = 6.62607015e-34  # Planck's constant (Joule second)
@@ -32,6 +36,8 @@ def observed_intensity(frequency,T1,T2,tau0,nu0,gamma):
     exponential = np.exp(-tau_nu(frequency,tau0,nu0,gamma))
     return planck_function_frequency(frequency,T1)*exponential+(1-exponential)*planck_function_frequency(frequency,T2)
 
+# This is the intensity you'd get assuming the presence of a homogeneous medium
+
 # Frequency range (in Hz)
 frequencies = np.linspace(4e14, 6e14, 5000)  # from 1 THz to 1000 THz (infrared to visible range)
 
@@ -51,7 +57,7 @@ for tau0 in [1,3,10]:
 ax.plot(frequencies * 1e-12, planck_function_frequency(frequencies,T1), linestyle='dotted', label='background T = {} K'.format(T1))
 ax.plot(frequencies * 1e-12, planck_function_frequency(frequencies,T2), linestyle='dotted', label='foreground T = {} K'.format(T2))
 plt.legend()
-plt.ylabel('Spectral Radiance (W/m^2/sr/Hz)')
+plt.ylabel(f'Spectral Radiance (W m$-^2$sr$^-1$Hz$^-1$)')
 plt.xlabel('Frequency (THz)')
 ax = ax.twinx()
 for tau0 in [1,3,10]:
@@ -61,6 +67,7 @@ plt.legend(loc='lower left')
 plt.ylabel('optical depth')
 
 plt.grid(True)
+plt.savefig("img1.pdf", transparent = True)
     
 # Temperature in Kelvin
 T1  = 5000
@@ -75,7 +82,7 @@ for tau0 in [1,3,10]:
 ax.plot(frequencies * 1e-12, planck_function_frequency(frequencies,T1), linestyle='dotted', label='background T = {} K'.format(T1))
 ax.plot(frequencies * 1e-12, planck_function_frequency(frequencies,T2), linestyle='dotted', label='foreground T = {} K'.format(T2))
 plt.legend()
-plt.ylabel('Spectral Radiance (W/m^2/sr/Hz)')
+plt.ylabel('Spectral Radiance (W m$^-2$sr$^-1$ Hz$^-1$)')
 plt.xlabel('Frequency (THz)')
 ax = ax.twinx()
 for tau0 in [1,3,10]:
@@ -85,4 +92,5 @@ plt.legend(loc='lower left')
 plt.ylabel('optical depth')
 
 plt.grid(True)
+plt.savefig("img2.pdf", transparent = True)
 plt.show()
